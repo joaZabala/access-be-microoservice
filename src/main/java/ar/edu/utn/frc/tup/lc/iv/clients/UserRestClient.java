b687696295a7c0edb1ddc6b2299c2267f8576484
404975-Zabala-joaquin <404975@tecnicatura.frc.utn.edu.ar>
2024-09-30 17:51:40 -0300

    config circuit breaker

--- src/main/java/ar/edu/utn/frc/tup/lc/iv/clients/UserRestClient.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/clients/UserRestClient.java
@@ -1,9 +1,11 @@
 package ar.edu.utn.frc.tup.lc.iv.clients;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -29,12 +31,23 @@ public class UserRestClient {
     private String userServiceUrl;
 
     /**
+     * name of circuit breaker.
+     */
+    private static final String INSTANCE_NAME = "user-service";
+
+    /**
+     *  name of circuit breaker fallback method.
+     */
+    private static  final String FALLBACK_METHOD = "fallback";
+
+    /**
      * Fetches user details by ID.
      *
      * @param id the user ID.
      * @return the UserDto if found.
      * @throws IllegalArgumentException if the user does not exist.
      */
+    @CircuitBreaker(name = INSTANCE_NAME, fallbackMethod = FALLBACK_METHOD)
     public UserDto getUserById(Long id) {
 
         ResponseEntity<UserDto> response =
@@ -45,6 +58,25 @@ public class UserRestClient {
         } else {
             throw new EntityNotFoundException("El usuario con el id " + id + " no existe");
         }
+    }
+
+    /**
+     * @param ex exeption captured.
+     * @return response entity with error.
+     */
+    public ResponseEntity<String> fallback(EntityNotFoundException ex) {
+        System.out.println("CircuitBreaker Fallback: " + ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    /**
+     * Circuit breaker fallback method.
+     * @param ex exeption captured.
+     * @return response entity with error.
+     */
+    public ResponseEntity<String> fallback(Exception ex) {
+        System.out.println("CircuitBreaker Fallback (General): " + ex.getMessage());
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Servicio no disponible, por favor intente más tarde.");
     }
 
     /**
--- src/main/java/ar/edu/utn/frc/tup/lc/iv/configs/WebConfig.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/configs/WebConfig.java
@@ -4,8 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Configures Cross-Origin Resource Sharing (CORS) for the application,
+ * allowing requests from the frontend running at 'http://localhost:4200'.
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    /**
+     * Adds CORS mappings to allow requests from
+     * specific origins with certain methods.
+     *
+     * @param registry the CorsRegistry to configure.
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
