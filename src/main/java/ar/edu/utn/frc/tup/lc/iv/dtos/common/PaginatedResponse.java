21e501bed1dca83ee626f3fe95339947fcc9582b
404975-Zabala-joaquin <404975@tecnicatura.frc.utn.edu.ar>
2024-10-21 10:48:51 -0300

    pmd coments

--- src/main/java/ar/edu/utn/frc/tup/lc/iv/dtos/common/PaginatedResponse.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/dtos/common/PaginatedResponse.java
@@ -7,11 +7,21 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+/**
+ * object used to return a paginated response.
+ * @param <T> the type of the items.
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PaginatedResponse<T> {
+    /**
+     * the list of items.
+     */
     private List<T> items;
+    /**
+     * the total number of elements.
+     */
     @JsonProperty("total_elements")
     private long totalElements;
 }
