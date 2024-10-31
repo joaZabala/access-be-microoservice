cd185532edbe0ee18265d230847507dee50d9021
113816-Tahan-Federico <128856709+113816-Tahan-Federico@users.noreply.github.com>
2024-10-31 13:23:59 -0300

    fix filters

--- src/main/java/ar/edu/utn/frc/tup/lc/iv/dtos/common/accesses/AccessesFilter.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/dtos/common/accesses/AccessesFilter.java
@@ -17,42 +17,34 @@ public class AccessesFilter {
      * Filter text to search in vehicle
      * registration and visitor details.
      */
-    @JsonProperty("text_filter")
     private String textFilter;
     /**
      * Start date for filtering access records.
      */
-    @JsonProperty("from_date")
     private LocalDate fromDate;
     /**
      * End date for filtering access records.
      */
-    @JsonProperty("to_date")
     private LocalDate toDate;
     /**
      * Type of document to filter the access records.
      */
-    @JsonProperty("document_type")
     private DocumentType documentType;
     /**
      * Type of vehicle to filter the access records.
      */
-    @JsonProperty("vehicle_type")
     private VehicleTypes vehicleType;
     /**
      * Type of action (e.g., enter, exit)
      * to filter the access records.
      */
-    @JsonProperty("action_type")
     private ActionTypes actionType;
     /**
      * Type of visitor to filter the access records.
      */
-    @JsonProperty("visitor_type")
     private VisitorType visitorType;
     /**
      * External ID to filter the access records.
      */
-    @JsonProperty("external_id")
     private Long externalId;
 }
