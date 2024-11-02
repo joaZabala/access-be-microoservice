1608f0b3c78609f251d864488578a5068f2c010b
113816-Tahan-Federico <128856709+113816-Tahan-Federico@users.noreply.github.com>
2024-11-02 18:08:05 -0300

    add accesses test

--- src/test/java/ar/edu/utn/frc/tup/lc/iv/services/imp/AccessesServiceTest.java
+++ src/test/java/ar/edu/utn/frc/tup/lc/iv/services/imp/AccessesServiceTest.java
@@ -2,8 +2,13 @@ package ar.edu.utn.frc.tup.lc.iv.services.imp;
 
 import ar.edu.utn.frc.tup.lc.iv.dtos.common.authorized.AccessDTO;
 import ar.edu.utn.frc.tup.lc.iv.entities.AccessEntity;
+import ar.edu.utn.frc.tup.lc.iv.entities.AuthEntity;
+import ar.edu.utn.frc.tup.lc.iv.entities.VisitorEntity;
 import ar.edu.utn.frc.tup.lc.iv.models.ActionTypes;
+import ar.edu.utn.frc.tup.lc.iv.models.DocumentType;
 import ar.edu.utn.frc.tup.lc.iv.repositories.AccessesRepository;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,29 +20,50 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class AccessesServiceTest {
-    @MockBean
+    @InjectMocks
     private AccessesService accessesService;
-    @MockBean
+    @Mock
     private AccessesRepository accessesRepository;
-    @Autowired
+    @Mock
     ModelMapper modelMapper;
     @Test
     void testGetAllExits() {
         AccessEntity access1 = new AccessEntity();
         access1.setActionDate(LocalDateTime.now().minusDays(1));
-        access1.setAction(ActionTypes.EXIT);
+        AuthEntity auth1 = new AuthEntity();
+        auth1.setCreatedUser(1L);
+        access1.setAuth(auth1);
 
-        AccessEntity access2= new AccessEntity();
+        AccessEntity access2 = new AccessEntity();
         access2.setActionDate(LocalDateTime.now());
-        access2.setAction(ActionTypes.EXIT);
+        VisitorEntity v1 = new VisitorEntity();
+        v1.setDocumentType(DocumentType.DNI);
+        AuthEntity auth2 = new AuthEntity();
+        auth2.setCreatedUser(1L);
+        access2.setAuth(auth2);
+        VisitorEntity v2 = new VisitorEntity();
+        v1.setDocumentType(DocumentType.DNI);
+        auth2.setVisitor(v2);
+        auth1.setVisitor(v1);
 
         when(accessesRepository.findByAction(ActionTypes.EXIT)).thenReturn(Arrays.asList(access1, access2));
 
+        when(modelMapper.map(any(AccessEntity.class), any())).thenAnswer(invocation -> {
+            AccessEntity accessEntity = invocation.getArgument(0);
+            AccessDTO accessDTO = new AccessDTO();
+            accessDTO.setActionDate(accessEntity.getActionDate());
+            accessDTO.setAuthorizerId(accessEntity.getAuth().getCreatedUser());
+            return accessDTO;
+        });
+
         List<AccessDTO> result = accessesService.getAllExits();
+        verify(accessesRepository).findByAction(ActionTypes.EXIT);
 
         assertEquals(2, result.size());
         assertEquals(access2.getActionDate(), result.get(0).getActionDate());
