bfa0652616523f5df4bfafe539959b4f222dcf26
113816-Tahan-Federico <128856709+113816-Tahan-Federico@users.noreply.github.com>
2024-11-02 20:21:54 -0300

    add tests

--- src/test/java/ar/edu/utn/frc/tup/lc/iv/services/imp/AccessesServiceTest.java
+++ src/test/java/ar/edu/utn/frc/tup/lc/iv/services/imp/AccessesServiceTest.java
@@ -10,6 +10,7 @@ import ar.edu.utn.frc.tup.lc.iv.models.VisitorType;
 import ar.edu.utn.frc.tup.lc.iv.repositories.AccessesRepository;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.internal.matchers.Any;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,6 +22,8 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -176,5 +179,93 @@ public class AccessesServiceTest {
 
         assertEquals(1, result.size());
         assertEquals(access2.getActionDate(), result.get(0).getActionDate());
+    }
+
+    @Test
+    void canDoActionSuccess() {
+
+        AccessEntity access2 = new AccessEntity();
+        access2.setVehicleReg("XXX - 123");
+        access2.setAction(ActionTypes.ENTRY);
+        access2.setActionDate(LocalDateTime.now());
+        VisitorEntity v1 = new VisitorEntity();
+        v1.setDocumentType(DocumentType.DNI);
+        AuthEntity auth2 = new AuthEntity();
+        auth2.setCreatedUser(1L);
+        access2.setAuth(auth2);
+        VisitorEntity v2 = new VisitorEntity();
+        auth2.setExternalID(1L);
+        v1.setDocumentType(DocumentType.DNI);
+        auth2.setVisitor(v2);
+        auth2.setVisitorType(VisitorType.VISITOR);
+
+        when(accessesRepository.findByVehicleReg("XXX - 123")).thenReturn(List.of(access2));
+
+        Boolean result = accessesService.canDoAction("XXX - 123", ActionTypes.EXIT);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void canDoActionFail() {
+
+        AccessEntity access2 = new AccessEntity();
+        access2.setVehicleReg("XXX - 123");
+        access2.setAction(ActionTypes.ENTRY);
+        access2.setActionDate(LocalDateTime.now());
+        VisitorEntity v1 = new VisitorEntity();
+        v1.setDocumentType(DocumentType.DNI);
+        AuthEntity auth2 = new AuthEntity();
+        auth2.setCreatedUser(1L);
+        access2.setAuth(auth2);
+        VisitorEntity v2 = new VisitorEntity();
+        auth2.setExternalID(1L);
+        v1.setDocumentType(DocumentType.DNI);
+        auth2.setVisitor(v2);
+        auth2.setVisitorType(VisitorType.VISITOR);
+
+        when(accessesRepository.findByVehicleReg("XXX - 123")).thenReturn(List.of(access2));
+
+        Boolean result = accessesService.canDoAction("XXX - 123", ActionTypes.ENTRY);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void canDoActionNull() {
+
+        when(accessesRepository.findByVehicleReg("XXX - 123")).thenReturn(List.of());
+
+        Boolean result = accessesService.canDoAction("XXX - 123", ActionTypes.ENTRY);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void registerAccess() {
+
+        AccessEntity access2 = new AccessEntity();
+        access2.setVehicleReg("XXX - 123");
+        access2.setAction(ActionTypes.ENTRY);
+        access2.setActionDate(LocalDateTime.now());
+        AuthEntity auth2 = new AuthEntity();
+        auth2.setCreatedUser(1L);
+        access2.setAuth(auth2);
+        VisitorEntity v2 = new VisitorEntity();
+        auth2.setExternalID(1L);
+        auth2.setVisitor(v2);
+        auth2.setVisitorType(VisitorType.VISITOR);
+
+        when(modelMapper.map(any(AccessEntity.class), any())).thenAnswer(invocation -> {
+            AccessEntity accessEntity = invocation.getArgument(0);
+            AccessDTO accessDTO = new AccessDTO();
+            accessDTO.setActionDate(accessEntity.getActionDate());
+            accessDTO.setAuthorizerId(accessEntity.getAuth().getCreatedUser());
+            return accessDTO;
+        });
+        when(accessesRepository.save(any(AccessEntity.class))).thenReturn(access2);
+
+        accessesService.registerAccess(access2);
+        verify(accessesRepository).save(access2);
     }
 }
