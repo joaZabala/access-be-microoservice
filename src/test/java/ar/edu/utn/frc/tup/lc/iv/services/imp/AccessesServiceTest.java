65b54e112ce1fdc2d04e913752183edc43aaacc4
113816-Tahan-Federico <128856709+113816-Tahan-Federico@users.noreply.github.com>
2024-11-02 22:18:07 -0300

    add test getallAceesses

--- src/test/java/ar/edu/utn/frc/tup/lc/iv/services/imp/AccessesServiceTest.java
+++ src/test/java/ar/edu/utn/frc/tup/lc/iv/services/imp/AccessesServiceTest.java
@@ -1,21 +1,30 @@
 package ar.edu.utn.frc.tup.lc.iv.services.imp;
 
+import ar.edu.utn.frc.tup.lc.iv.clients.UserDetailDto;
+import ar.edu.utn.frc.tup.lc.iv.clients.UserDto;
+import ar.edu.utn.frc.tup.lc.iv.clients.UserRestClient;
+import ar.edu.utn.frc.tup.lc.iv.dtos.common.PaginatedResponse;
+import ar.edu.utn.frc.tup.lc.iv.dtos.common.accesses.AccessesFilter;
 import ar.edu.utn.frc.tup.lc.iv.dtos.common.authorized.AccessDTO;
 import ar.edu.utn.frc.tup.lc.iv.entities.AccessEntity;
 import ar.edu.utn.frc.tup.lc.iv.entities.AuthEntity;
 import ar.edu.utn.frc.tup.lc.iv.entities.VisitorEntity;
 import ar.edu.utn.frc.tup.lc.iv.models.ActionTypes;
 import ar.edu.utn.frc.tup.lc.iv.models.DocumentType;
+import ar.edu.utn.frc.tup.lc.iv.models.VehicleTypes;
 import ar.edu.utn.frc.tup.lc.iv.models.VisitorType;
 import ar.edu.utn.frc.tup.lc.iv.repositories.AccessesRepository;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.internal.matchers.Any;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
@@ -30,12 +39,21 @@ import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class AccessesServiceTest {
-    @InjectMocks
+    @SpyBean
     private AccessesService accessesService;
-    @Mock
+    @MockBean
     private AccessesRepository accessesRepository;
-    @Mock
-    ModelMapper modelMapper;
+    @MockBean
+    private UserRestClient userRestClient;
+
+    private final String userService = "http://host.docker.internal:8283/users";
+
+    @MockBean
+    private RestTemplate restTemplate;
+    @BeforeEach
+    public void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
     @Test
     void testGetAllExits() {
         AccessEntity access1 = new AccessEntity();
@@ -58,13 +76,6 @@ public class AccessesServiceTest {
 
         when(accessesRepository.findByAction(ActionTypes.EXIT)).thenReturn(Arrays.asList(access1, access2));
 
-        when(modelMapper.map(any(AccessEntity.class), any())).thenAnswer(invocation -> {
-            AccessEntity accessEntity = invocation.getArgument(0);
-            AccessDTO accessDTO = new AccessDTO();
-            accessDTO.setActionDate(accessEntity.getActionDate());
-            accessDTO.setAuthorizerId(accessEntity.getAuth().getCreatedUser());
-            return accessDTO;
-        });
 
         List<AccessDTO> result = accessesService.getAllExits();
         verify(accessesRepository).findByAction(ActionTypes.EXIT);
@@ -96,13 +107,6 @@ public class AccessesServiceTest {
 
         when(accessesRepository.findByAction(ActionTypes.ENTRY)).thenReturn(Arrays.asList(access1, access2));
 
-        when(modelMapper.map(any(AccessEntity.class), any())).thenAnswer(invocation -> {
-            AccessEntity accessEntity = invocation.getArgument(0);
-            AccessDTO accessDTO = new AccessDTO();
-            accessDTO.setActionDate(accessEntity.getActionDate());
-            accessDTO.setAuthorizerId(accessEntity.getAuth().getCreatedUser());
-            return accessDTO;
-        });
 
         List<AccessDTO> result = accessesService.getAllEntries();
         verify(accessesRepository).findByAction(ActionTypes.ENTRY);
@@ -135,13 +139,6 @@ public class AccessesServiceTest {
 
         when(accessesRepository.findByAuthVisitorTypeAndAuthExternalID(VisitorType.VISITOR, 1L)).thenReturn(Arrays.asList(access2));
 
-        when(modelMapper.map(any(AccessEntity.class), any())).thenAnswer(invocation -> {
-            AccessEntity accessEntity = invocation.getArgument(0);
-            AccessDTO accessDTO = new AccessDTO();
-            accessDTO.setActionDate(accessEntity.getActionDate());
-            accessDTO.setAuthorizerId(accessEntity.getAuth().getCreatedUser());
-            return accessDTO;
-        });
 
         List<AccessDTO> result = accessesService.getAllAccessByTypeAndExternalID(VisitorType.VISITOR, 1L);
 
@@ -167,13 +164,6 @@ public class AccessesServiceTest {
 
         when(accessesRepository.findByAuthVisitorType(VisitorType.VISITOR)).thenReturn(Arrays.asList(access2));
 
-        when(modelMapper.map(any(AccessEntity.class), any())).thenAnswer(invocation -> {
-            AccessEntity accessEntity = invocation.getArgument(0);
-            AccessDTO accessDTO = new AccessDTO();
-            accessDTO.setActionDate(accessEntity.getActionDate());
-            accessDTO.setAuthorizerId(accessEntity.getAuth().getCreatedUser());
-            return accessDTO;
-        });
 
         List<AccessDTO> result = accessesService.getAllAccessByType(VisitorType.VISITOR);
 
@@ -256,16 +246,54 @@ public class AccessesServiceTest {
         auth2.setVisitor(v2);
         auth2.setVisitorType(VisitorType.VISITOR);
 
-        when(modelMapper.map(any(AccessEntity.class), any())).thenAnswer(invocation -> {
-            AccessEntity accessEntity = invocation.getArgument(0);
-            AccessDTO accessDTO = new AccessDTO();
-            accessDTO.setActionDate(accessEntity.getActionDate());
-            accessDTO.setAuthorizerId(accessEntity.getAuth().getCreatedUser());
-            return accessDTO;
-        });
         when(accessesRepository.save(any(AccessEntity.class))).thenReturn(access2);
 
         accessesService.registerAccess(access2);
         verify(accessesRepository).save(access2);
+    }
+
+    @Test
+    void testGetAllAccess() {
+        AccessesFilter filter = new AccessesFilter();
+        filter.setFromDate(null);
+        filter.setToDate(null);
+        filter.setTextFilter(null);
+        filter.setExternalId(null);
+        filter.setActionType(ActionTypes.ENTRY);
+        filter.setDocumentType(DocumentType.DNI);
+        filter.setVehicleType(VehicleTypes.CAR);
+        filter.setVisitorType(VisitorType.VISITOR);
+
+        AccessEntity accessEntity = new AccessEntity();
+        AuthEntity auth = new AuthEntity();
+        VisitorEntity v2 = new VisitorEntity();
+        v2.setVisitorId(1L);
+        auth.setAuthId(1L);
+        auth.setCreatedUser(1L);
+        v2.setCreatedUser(1L);
+        accessEntity.setVehicleReg("XXXXXX");
+        accessEntity.setCreatedUser(1L);
+        accessEntity.setVehicleType(VehicleTypes.CAR);
+        v2.setDocumentType(DocumentType.DNI);
+        auth.setVisitorType(VisitorType.VISITOR);
+        accessEntity.setAction(ActionTypes.ENTRY);
+        auth.setVisitor(v2);
+        accessEntity.setAuth(auth);
+
+        Page<AccessEntity> accessPage = new PageImpl<>(List.of(accessEntity));
+
+        UserDetailDto userDetailDto = new UserDetailDto();
+        userDetailDto.setId(1L);
+        userDetailDto.setFirstName("John");
+        userDetailDto.setLastName("Doe");
+
+
+        when(accessesRepository.findAll(any(Specification.class), any(Pageable.class))).thenReturn(accessPage);
+        when(userRestClient.getUsersByIds(List.of(1L))).thenReturn(List.of(userDetailDto));
+
+        PaginatedResponse<AccessDTO> response = accessesService.getAllAccess(filter, 0, 10);
+
+        assertEquals(1, response.getItems().size());
+        AccessDTO accessDTO = response.getItems().get(0);
     }
 }
